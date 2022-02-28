0c749d6f683483e28f981d80e80e3aeb6bef0bce
shimun <shimun.eps@gmail.com>
2022-02-28 15:33:45 +0600

    Mobile reponse part2

--- resources/views/frontend/Homepage/index.blade.php
+++ resources/views/frontend/Homepage/index.blade.php
@@ -46,10 +46,10 @@
                         <div class="col-12 col-md-3 col-lg-6 row">
                             <!-- Welcome Thumb -->
                             {{-- <div class="welcome-thumb text-center" data-aos="fade-left" data-aos-delay="500" data-aos-duration="1000"> --}}
-                                <img class="mobimg" src="{{ URL::to('') }}/frontend/images/web2.png" alt=""style="position: relative">
+                                <img class="mobimg" src="{{ URL::to('') }}/frontend/images/web2.png" data-aos="fade-left" data-aos-delay="500" data-aos-duration="1000"   alt="">
                                 {{-- <img style="margin-left:200px; width:280px" class="mobimg" src="{{ URL::to('') }}/frontend/images/mobile-slider.png" alt=""> --}}
 
-            <img src="{{ URL::to('/') }}/frontend/images/r1test.gif" alt="Test"   style="position:absolute;top:36.5%;width:31%; height:310px;margin-left:32.35%">
+            <img src="{{ URL::to('/') }}/frontend/images/r1test.gif" alt="Test" class="mobgifimg"  data-aos="fade-left" data-aos-delay="500" data-aos-duration="1000">
           {{--   <video controls autoplay>
                 <source src="test.mp4" type="video/mp4">
 
